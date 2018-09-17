e0258b55f994009446418bfa255a021e8b846e5d
Johannes Diemke <johannesdiemke128@gmail.com>
2018-09-17 21:08:00 +0100

    Add logging to computation controller

--- computation-service/src/main/java/io/github/jdiemke/springcloud/ComputationController.java
+++ computation-service/src/main/java/io/github/jdiemke/springcloud/ComputationController.java
@@ -1,13 +1,18 @@
 package io.github.jdiemke.springcloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ComputationController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ComputationController.class);
+
     @RequestMapping("/")
     public String getName() {
+        logger.info("Computing Name ...");
         return "Johannes";
     }
 
